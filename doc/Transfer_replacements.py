986dae0e970f2c027cf202f201529b3563b87100
Amrein Anja <anja.amrein@swisstopo.ch>
2022-05-06 11:35:06 +0200

    update of xml object list by validation check; C1-C4 tables added

---
+++ doc/Transfer_replacements.py
@@ -0,0 +1,12 @@
+# Replacements among excel list
+
+
+################################
+## IMPORTS
+################################
+import xml.etree.ElementTree as ET
+tree = ET.parse("GeologyModelLookUp_V3_0.xml")
+root = tree.getroot()
+
+
+################################
